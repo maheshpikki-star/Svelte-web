e8659f34c9f835dfe2478933c7d6eae11bc8d5c7
Mahesh Pikki <mahesh.pikki@apxor.com>
2026-05-04 16:33:11 +0530

    removed unsafe-inline

--- svelte.config.js
+++ svelte.config.js
@@ -2,25 +2,25 @@
 // import adapter from '@sveltejs/adapter-auto';
 
 export default {
-  // kit: {
-  //   adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-  //   csp: {
-  //     mode: 'auto', 
-  //     directives: {
-  //       'default-src': ['self'],
-  //       // 1. Allow the SDK scripts to run
-  //       'script-src': ['self', 'https://*.apxor.com', "'unsafe-inline'", "'unsafe-eval'"],
+    csp: {
+      mode: 'auto', 
+      directives: {
+        'default-src': ['self'],
+        // 1. Allow the SDK scripts to run
+        'script-src': ['self', 'https://*.apxor.com'],
         
-  //       // 2. Allow styles from the SDK and your inline JS-generated styles
-  //       'style-src': ['self', "'unsafe-inline'", 'https://*.apxor.com'], 
+        // 2. Allow styles from the SDK and your inline JS-generated styles
+        'style-src': ['self', 'https://*.apxor.com'], 
         
-  //       // 3. Allow gradients and images from the SDK
-  //       'img-src': ['self', 'data:', 'https://*.apxor.com'],
+        // 3. Allow gradients and images from the SDK
+        'img-src': ['self', 'data:', 'https://*.apxor.com'],
         
-  //       // 4. CRITICAL: Allow the API call you shared earlier
-  //       'connect-src': ['self', 'https://server.apxor.com', 'https://*.apxor.com']
-  //     }
-  //   }
-  // }
+        // 4. CRITICAL: Allow the API call you shared earlier
+        'connect-src': ['self', 'https://*.apxor.com']
+      }
+    }
+  }
 };
