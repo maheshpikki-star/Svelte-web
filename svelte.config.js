9f4e8ba5f97c5da4b2bf0818c05a5bea0bc84d6a
Mahesh Pikki <mahesh.pikki@apxor.com>
2026-05-04 15:07:09 +0530

    removed config

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,26 @@
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
 export default {
-  kit: {
-    adapter: adapter(),
+  // kit: {
+  //   adapter: adapter(),
 
-    csp: {
-      mode: 'auto', 
-      directives: {
-        'default-src': ['self'],
-        // 1. Allow the SDK scripts to run
-        'script-src': ['self', 'https://*.apxor.com', "'unsafe-inline'", "'unsafe-eval'"],
+  //   csp: {
+  //     mode: 'auto', 
+  //     directives: {
+  //       'default-src': ['self'],
+  //       // 1. Allow the SDK scripts to run
+  //       'script-src': ['self', 'https://*.apxor.com', "'unsafe-inline'", "'unsafe-eval'"],
         
-        // 2. Allow styles from the SDK and your inline JS-generated styles
-        'style-src': ['self', "'unsafe-inline'", 'https://*.apxor.com'], 
+  //       // 2. Allow styles from the SDK and your inline JS-generated styles
+  //       'style-src': ['self', "'unsafe-inline'", 'https://*.apxor.com'], 
         
-        // 3. Allow gradients and images from the SDK
-        'img-src': ['self', 'data:', 'https://*.apxor.com'],
+  //       // 3. Allow gradients and images from the SDK
+  //       'img-src': ['self', 'data:', 'https://*.apxor.com'],
         
-        // 4. CRITICAL: Allow the API call you shared earlier
-        'connect-src': ['self', 'https://server.apxor.com', 'https://*.apxor.com']
-      }
-    }
-  }
+  //       // 4. CRITICAL: Allow the API call you shared earlier
+  //       'connect-src': ['self', 'https://server.apxor.com', 'https://*.apxor.com']
+  //     }
+  //   }
+  // }
 };
