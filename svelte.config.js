e43eba065c769658763dfab99fcd6524754cf0bf
Mahesh Pikki <mahesh.pikki@apxor.com>
2026-05-04 17:04:20 +0530

    removed domain

--- svelte.config.js
+++ svelte.config.js
@@ -10,16 +10,16 @@ export default {
       directives: {
         'default-src': ['self'],
         // 1. Allow the SDK scripts to run
-        'script-src': ['self', 'https://*.apxor.com'],
+        'script-src': ['self'],
         
         // 2. Allow styles from the SDK and your inline JS-generated styles
-        'style-src': ['self', 'https://*.apxor.com'], 
+        'style-src': ['self'], 
         
         // 3. Allow gradients and images from the SDK
-        'img-src': ['self', 'data:', 'https://*.apxor.com'],
+        'img-src': ['self', 'data:'],
         
         // 4. CRITICAL: Allow the API call you shared earlier
-        'connect-src': ['self', 'https://*.apxor.com']
+        'connect-src': ['self']
       }
     }
   }
