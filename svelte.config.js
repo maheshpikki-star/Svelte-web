0f9dab6aa765bebe1356e42c376cdb94c87d0500
Mahesh Pikki <mahesh.pikki@apxor.com>
2026-05-05 12:40:35 +0530

    added vercel and removed sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -4,18 +4,18 @@ import adapter from "@sveltejs/adapter-auto";
 const domains = ["https://*.apxor.com"];
 
 export default {
-  kit: {
-    adapter: adapter(),
+  // kit: {
+  //   adapter: adapter(),
 
-    csp: {
-      mode: "auto",
-      directives: {
-        "default-src": ['self'],
-        "script-src": ['self'],
-        "style-src": ['self'],
-        "img-src": ['self'],
-        "connect-src": ['self'],
-      },
-    },
-  },
+  //   csp: {
+  //     mode: "auto",
+  //     directives: {
+  //       "default-src": ['self'],
+  //       "script-src": ['self'],
+  //       "style-src": ['self'],
+  //       "img-src": ['self', 'data:'],
+  //       "connect-src": ['self'],
+  //     },
+  //   },
+  // },
 };
