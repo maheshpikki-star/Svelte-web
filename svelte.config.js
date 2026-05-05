ada484acb4d702a39f231c632a1a278ef13d1893
Mahesh Pikki <mahesh.pikki@apxor.com>
2026-05-05 11:54:59 +0530

    updated single quotes

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,21 @@
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-auto";
 
-const domains = ["https://*.apxor.com", "unsafe-inline"]
+const domains = ["https://*.apxor.com"];
 
 export default {
   kit: {
     adapter: adapter(),
 
     csp: {
-      mode: 'nonce',
+      mode: "nonce",
       directives: {
-        'default-src': ['self', ...domains],
-        'script-src': ['self', ...domains],
-        'style-src': ['self', ...domains],
-        'img-src': ['self', 'data:', ...domains],
-        'connect-src': ['self', ...domains]
-      }
-    }
-  }
+        "default-src": ["self"],
+        "script-src": ["self", ...domains],
+        "style-src": ["self", 'unsafe-inline', ...domains],
+        "img-src": ["self", "data:", ...domains],
+        "connect-src": ["self", ...domains],
+      },
+    },
+  },
 };
